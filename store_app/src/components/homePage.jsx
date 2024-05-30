466c8994646abcf10fa4ca04a7fcd50fa9943606
kalpana123-1 <kalpanatamatam1822000@gmail.com>
2024-05-30 10:06:16 +0530

    Home page modifications

--- store_app/src/components/homePage.jsx
+++ store_app/src/components/homePage.jsx
@@ -1,30 +1,42 @@
 import React from "react";
-import Cart from "../images/cart.png";
-import Male from "../images/male.png";
-import Female from "../images/female.png";
+import NavBar from "./navBar";
+// import Cart from "../images/cart.png";
+// import Male from "../images/male.png";
+// import Female from "../images/female.png";
 
 function HomePage() {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
   return (
     <div>
-      <h1>Welcome to Store App</h1>
-      <div
+      <div style={{ display: "flex" }}>
+        <NavBar />
+        <div style={{ marginLeft: "150px" }}>
+          <h1>Welcome to Store App</h1>
+          <p>Discover the best products at unbeatable prices.</p>
+        </div>
+      </div>
+
+      {/* <div
         style={{
           display: "flex",
           gap: "20%",
           alignItems: "center",
           margin: "10% 5%",
         }}
-      >
-        <img style={{ width: "10%" }} src={Male} alt="Male icon" />
-        <img style={{ width: "10%" }} src={Female} alt="Female icon" />
-        <img style={{ width: "10%" }} src={Cart} alt="Cart icon" />
-      </div>
-      <p style={{ textAlign: "center", fontSize: "20px", margin: "4% 2%" }}>
+      > */}
+      {/* <img style={{ width: "2%" }} src={Male} alt="Male icon" />
+        <img style={{ width: "2%" }} src={Female} alt="Female icon" />
+        <img style={{ width: "2%" }} src={Cart} alt="Cart icon" /> */}
+      {/* </div> */}
+      {/* <p style={{ textAlign: "center", fontSize: "20px", margin: "4% 2%" }}>
         We're delighted to have you here. Explore our wide range of products and
         find everything you need in one place. Whether you're looking for the
         latest gadgets, fashionable clothing, or everyday essentials, we've got
         you covered.
-      </p>
+      </p> */}
     </div>
   );
 }
