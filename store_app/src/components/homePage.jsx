7e28a626724b6d543a70bd675071fb39e50fd0b6
kalpana123-1 <kalpanatamatam1822000@gmail.com>
2024-06-03 15:41:22 +0530

    Home page navigation bar modifications

--- store_app/src/components/homePage.jsx
+++ store_app/src/components/homePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import NavBar from "./navBar";
+import { NavBar } from "./navBar";
 import "../css/homePage.css";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
--- store_app/src/components/navBar.jsx
+++ store_app/src/components/navBar.jsx
@@ -1,27 +1,47 @@
 import React from "react";
 import "../css/navBar.css";
-// import Cart from "../images/cart.png";
+import { useNavigate } from "react-router-dom";
+import Cart from "../images/cart.png";
 
 function NavBar() {
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    navigate("/");
+  };
   return (
     <div className="Navbar">
-      <h2>Products</h2>
-      <h2>
+      <h2 style={{ cursor: "pointer" }} onClick={() => navigate("/home")}>
+        Products
+      </h2>
+      <h2 onClick={() => navigate("/cart")} style={{ cursor: "pointer" }}>
         Cart
-        {/* <img
+        <img
           style={{
-            width: "80px",
+            width: "25px",
             marginLeft: "10px",
             marginBottom: "-5px",
           }}
           src={Cart}
           alt="Cart icon"
-        /> */}
+        />
       </h2>
       <h2>Users</h2>
-      <h2>Logout</h2>
+      <h2 onClick={handleLogout} style={{ cursor: "pointer" }}>
+        Logout
+      </h2>
     </div>
   );
 }
 
-export default NavBar;
+function UserNavBar() {
+  return (
+    <div className="Navbar">
+      <h2>View All Users</h2>
+      <h2>Add User</h2>
+      <h2>Update User</h2>
+      <h2>Delete User</h2>
+    </div>
+  );
+}
+
+export { NavBar, UserNavBar };
