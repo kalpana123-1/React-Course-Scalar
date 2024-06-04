fa355e1f6f8cdc83d03aff81f800d4d9233c3ab2
kalpana123-1 <kalpanatamatam1822000@gmail.com>
2024-06-04 10:32:46 +0530

    Users List API integration is done

--- store_app/src/components/Users.jsx
+++ store_app/src/components/Users.jsx
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 // import { useNavigate } from 'react-router-dom';
 import { UserNavBar } from "./navBar";
 import axios from "axios";
+import '../css/users.css';
+import UserIcon from '../images/usericon.webp';
 
 function Users() {
   const [users, setUsers] = useState([]);
@@ -10,7 +12,7 @@ function Users() {
   useEffect(() => {
     setLoading(true); // Set loading state to true before making the API call
     axios
-      .get("https://fakestoreapi.com/users")
+      .get('https://fakestoreapi.com/users')
       .then((res) => {
         setUsers(res.data);
         setLoading(false); // Set loading state to false after successful API call
@@ -22,37 +24,49 @@ function Users() {
   }, []);
 
   return (
-    <div style={{ display: "flex", alignItems: "flex-start" }}>
+    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
       <UserNavBar />
 
       {loading ? (
         <div>Loading...</div>
       ) : (
-        <div className="user-card">
-          <h1>Deatils of Users</h1>
-          {users.map((user) => (
-            <div key={user.id}>
-              <strong>Firstname:</strong> {user.name.firstname}
-              <br />
-              <strong>Lastname:</strong> {user.name.lastname}
-              <strong>Email:</strong> {user.email}
-              <br />
-              <strong>Username:</strong> {user.username}
-              <br />
-              <strong>Phone:</strong> {user.phone}
-              <br />
-              <strong>Address:</strong>
-              <br />
-              {user.address.street}
-              <br />
-              {user.address.number},
-              <br />
-              {user.address.city}, {user.address.zipcode}
-              <br />
-              {user.address.geolocation.lat}, {user.address.geolocation.long}
-              <br />
-            </div>
-          ))}
+        <div style={{ marginLeft: '70px', marginTop: '20px' }}>
+          <div className="user-card">
+            {users.map((user) => (
+              <div key={user.id} className="user-details">
+                <img
+                  src={UserIcon}
+                  alt="User Icon"
+                  style={{
+                    width: '100px',
+                    height: '100px',
+                    paddingLeft: '30px',
+                  }}
+                />
+                <br />
+                <strong>Firstname:</strong> {user.name.firstname}
+                <br />
+                <strong>Lastname:</strong> {user.name.lastname}
+                <br />
+                <strong>Email:</strong> {user.email}
+                <br />
+                <strong>Username:</strong> {user.username}
+                <br />
+                <strong>Phone:</strong> {user.phone}
+                <br />
+                <strong>Address:</strong>
+                <br />
+                {user.address.street}
+                <br />
+                {user.address.number},
+                <br />
+                {user.address.city}, {user.address.zipcode}
+                <br />
+                {user.address.geolocation.lat}, {user.address.geolocation.long}
+                <br />
+              </div>
+            ))}
+          </div>
         </div>
       )}
     </div>
